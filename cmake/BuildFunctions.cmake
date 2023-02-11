9e7336041ec312d3f00d6d7e49246eb96db0c7b5
love2hina <webmaster@love2hina.net>
2023-02-11 16:31:23 +0900

    CMake再作成中

--- cmake/BuildFunctions.cmake
+++
@@ -1,6 +0,0 @@
-########################################
-# FUNCTION project_group
-########################################
-function(project_group target name)
-    set_target_properties(${target} PROPERTIES FOLDER ${name})
-endfunction(project_group)
