5873530b0adf9f95bde5bd0aa9919c15e035d08f
love2hina <webmaster@love2hina.net>
2023-02-11 11:17:15 +0900

    組み込み用にCMake再作成中

---
+++ cmake/BuildFunctions.cmake
@@ -0,0 +1,6 @@
+########################################
+# FUNCTION project_group
+########################################
+function(project_group target name)
+    set_target_properties(${target} PROPERTIES FOLDER ${name})
+endfunction(project_group)
